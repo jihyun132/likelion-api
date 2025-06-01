9673ff9b6459eb4d60f3cb017116ffaa2ab1bbe3
jihyun132 <jihyun.q.s@gmail.com>
2025-06-02 01:22:40 +0900

    design: Home 페이지 디자인

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,22 @@
 import { useContext } from "react";
 import { Link } from "react-router-dom";
 import AuthContext from "../contexts/AuthContext";
+import yulikelion from "../assets/yulikelion.png"
 
 /**메인 페이지 */
 const Home = () => {
   const { accessToken, logout } = useContext(AuthContext);
 
   return (
-    <div className="flex h-screen w-screen items-center justify-center">
+    <div className="font-['Tenada'] flex flex-col h-screen w-screen items-center justify-center space-y-3 bg-gradient-to-l from-purple-200 to-pink-100">
       {!accessToken ? (
         <>
-          <Link to={"/sign-in"} className="link">
+          <div className="font-['Tenada'] text-5xl text-gray-800">🦁 반가워요! 👋</div>
+          <img src={yulikelion} alt="yu-likelion" className="w-56 rounded-full my-8"/>
+          <Link to={"/sign-in"} className="w-xs bg-black text-white p-2 rounded-xl text-center hover:bg-gray-800 transition">
             로그인
           </Link>
-          <Link to={"/sign-up"} className="link">
+          <Link to={"/sign-up"} className="link rounded-xl w-xs bg-white hover:bg-gray-100 transition">
             회원가입
           </Link>
         </>
