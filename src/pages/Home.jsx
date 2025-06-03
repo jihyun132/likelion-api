a88aad6e1bd702f3dfdc17e5b4306d612c27e409
jihyun132 <jihyun.q.s@gmail.com>
2025-06-03 16:43:32 +0900

    design: 로그인 후 Home 페이지 디자인

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import { useContext } from "react";
 import { Link } from "react-router-dom";
 import AuthContext from "../contexts/AuthContext";
-import yulikelion from "../assets/yulikelion.png"
+import yulikelion from "../assets/yulikelion.png";
 
 /**메인 페이지 */
 const Home = () => {
@@ -11,24 +11,47 @@ const Home = () => {
     <div className="font-['Tenada'] flex flex-col h-screen w-screen items-center justify-center space-y-3 bg-gradient-to-l from-purple-200 to-pink-100">
       {!accessToken ? (
         <>
-          <div className="font-['Tenada'] text-5xl text-gray-800">🦁 반가워요! 👋</div>
-          <img src={yulikelion} alt="yu-likelion" className="w-56 rounded-full mt-5 mb-8"/>
-          <Link to={"/sign-in"} className="w-xs bg-black text-white p-2 rounded-xl text-center hover:bg-gray-800 transition">
+          <div className="font-['Tenada'] text-5xl text-gray-800">
+            🦁 반가워요! 👋
+          </div>
+          <img
+            src={yulikelion}
+            alt="yu-likelion"
+            className="w-56 rounded-full mt-5 mb-8"
+          />
+          <Link
+            to={"/sign-in"}
+            className="w-xs bg-black text-white p-2 rounded-xl text-center hover:bg-gray-800 transition"
+          >
             로그인
           </Link>
-          <Link to={"/sign-up"} className="link rounded-xl w-xs bg-white hover:bg-gray-100 transition">
+          <Link
+            to={"/sign-up"}
+            className="link rounded-xl w-xs bg-white hover:bg-gray-100 transition"
+          >
             회원가입
           </Link>
         </>
       ) : (
         <>
-          <button onClick={logout}>로그아웃</button>
-          <Link to={"/user-info"} className="link">
+          <Link
+            to={"/user-info"}
+            className="w-xs bg-black text-white  hover:bg-indigo-800 p-2 rounded-xl text-center transition"
+          >
             나의 정보 보기
           </Link>
-          <Link to={"/user-list"} className="link">
+          <Link
+            to={"/user-list"}
+            className="w-xs bg-black text-white  hover:bg-indigo-800 p-2 rounded-xl text-center transition"
+          >
             전체 회원 조회
           </Link>
+          <button
+            onClick={logout}
+            className="w-xs mt-5 bg-white text-red-500 p-2 rounded-xl text-center hover:bg-red-800 transition"
+          >
+            로그아웃
+          </button>
         </>
       )}
     </div>
